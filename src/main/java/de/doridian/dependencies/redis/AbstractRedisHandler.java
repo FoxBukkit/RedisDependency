749061e0a089dc261fd0aa9bfe56a6c0b744e00c
Doridian Draconia <mriq91@gmail.com>
2014-05-18 15:13:42 +0200

    Initial

---
+++ src/main/java/de/doridian/dependencies/redis/AbstractRedisHandler.java
@@ -0,0 +1,40 @@
+package de.doridian.dependencies.redis;
+
+import redis.clients.jedis.JedisPubSub;
+
+public abstract class AbstractRedisHandler extends JedisPubSub {
+	protected AbstractRedisHandler(final String channelName) {
+        final JedisPubSub _this = this;
+        Thread t = new Thread() {
+            public void run() {
+                while(true) {
+                    try {
+                        Thread.sleep(1000);
+                        RedisManager.subscribe(channelName, _this);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        t.setDaemon(true);
+        t.start();
+	}
+
+    protected abstract void onMessage(final String message) throws Exception;
+
+	@Override
+	public final void onMessage(final String channel, final String c_message) {
+		try {
+            onMessage(c_message);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+    @Override public final void onPMessage(String pattern, String channel, String message) { }
+    @Override public final void onSubscribe(String channel, int subscribedChannels) { }
+    @Override public final void onUnsubscribe(String channel, int subscribedChannels) { }
+    @Override public final void onPUnsubscribe(String pattern, int subscribedChannels) { }
+    @Override public final void onPSubscribe(String pattern, int subscribedChannels) { }
+}
---
+++ src/main/java/de/doridian/dependencies/redis/CacheMap.java
@@ -0,0 +1,208 @@
+package de.doridian.dependencies.redis;
+
+import redis.clients.jedis.JedisPubSub;
+
+import java.util.*;
+
+public class CacheMap implements Map<String, String> {
+    private final long expiryTime;
+    private final String name;
+
+    private final JedisPubSubListener jedisPubSubListener;
+
+    private class JedisPubSubListener extends AbstractRedisHandler {
+        private JedisPubSubListener(String channelName) {
+            super(channelName);
+        }
+
+        @Override
+        public void onMessage(final String c_message) {
+            final String[] msgSplit = c_message.split("\0");
+            synchronized (internalMap) {
+                switch (msgSplit.length) {
+                    case 1:
+                        if(msgSplit[0].charAt(0) == '\1')
+                            internalMap.clear();
+                        else
+                            internalMap.remove(msgSplit[0]);
+                        break;
+                    case 2:
+                        internalMap.put(msgSplit[0], new CacheEntry(msgSplit[1]));
+                        break;
+                }
+            }
+        }
+    }
+
+    public CacheMap(final long expiryTime, final String _name, final Map<String, String> parentMap) {
+        this.expiryTime = expiryTime;
+        this.parentMap = parentMap;
+        this.name = "cachemap_changes:" + _name;
+
+        Thread cleanupThread = new Thread() {
+            @Override
+            public void run() {
+                while(true) {
+                    try {
+                        Thread.sleep(expiryTime / 2L);
+                        final long currentTime = System.currentTimeMillis();
+                        synchronized (internalMap) {
+                            final Set<String> keysToRemove = new HashSet<>();
+                            for(Entry<String, CacheEntry> entry : internalMap.entrySet())
+                                if(entry.getValue().expiry < currentTime)
+                                    keysToRemove.add(entry.getKey());
+                            for(String key : keysToRemove)
+                                internalMap.remove(key);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        cleanupThread.setDaemon(true);
+        cleanupThread.start();
+
+        this.jedisPubSubListener = new JedisPubSubListener(this.name);
+    }
+
+    private class CacheEntry {
+        private final String data;
+        private final long expiry;
+        private CacheEntry(String data) {
+            this.data = data;
+            this.expiry = System.currentTimeMillis() + expiryTime;
+        }
+
+        private boolean isExpired() {
+            return expiry < System.currentTimeMillis();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || !(o instanceof CacheEntry)) return false;
+
+            CacheEntry that = (CacheEntry) o;
+
+            if (data != null ? !data.equals(that.data) : that.data != null) return false;
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            return data != null ? data.hashCode() : 0;
+        }
+    }
+    private final HashMap<String, CacheEntry> internalMap =  new HashMap<>();
+    private final Map<String, String> parentMap;
+
+    @Override
+    public int size() {
+        synchronized (parentMap) {
+            return parentMap.size();
+        }
+    }
+
+    @Override
+    public boolean isEmpty() {
+        synchronized (parentMap) {
+            return parentMap.isEmpty();
+        }
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        synchronized (parentMap) {
+            return parentMap.containsKey(key);
+        }
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        synchronized (parentMap) {
+            return parentMap.containsValue(value);
+        }
+    }
+
+    @Override
+    public String get(Object key) {
+        CacheEntry cacheEntry;
+        synchronized (internalMap) {
+            cacheEntry = internalMap.get(key);
+        }
+        if(cacheEntry != null && !cacheEntry.isExpired())
+            return cacheEntry.data;
+        final String value;
+        synchronized (parentMap) {
+            value = parentMap.get(key);
+        }
+        cacheEntry = new CacheEntry(value);
+        synchronized (internalMap) {
+            internalMap.put(key.toString(), cacheEntry);
+        }
+        return value;
+    }
+
+    @Override
+    public String put(String key, String value) {
+        synchronized (internalMap) {
+            internalMap.put(key, new CacheEntry(value));
+        }
+        RedisManager.publish(name, key + '\0' + value);
+        synchronized (parentMap) {
+            return parentMap.put(key, value);
+        }
+    }
+
+    @Override
+    public String remove(Object key) {
+        synchronized (internalMap) {
+            internalMap.remove(key);
+        }
+        RedisManager.publish(name, key.toString());
+        synchronized (parentMap) {
+            return parentMap.remove(key);
+        }
+    }
+
+    @Override
+    public void putAll(Map<? extends String, ? extends String> m) {
+        for(Entry<? extends String,? extends String> entry : m.entrySet()) {
+            put(entry.getKey(), entry.getValue());
+        }
+    }
+
+    @Override
+    public void clear() {
+        synchronized (internalMap) {
+            internalMap.clear();
+        }
+        RedisManager.publish(name, "\1");
+        synchronized (parentMap) {
+            parentMap.clear();
+        }
+    }
+
+    @Override
+    public Set<String> keySet() {
+        synchronized (parentMap) {
+            return parentMap.keySet();
+        }
+    }
+
+    @Override
+    public Collection<String> values() {
+        synchronized (parentMap) {
+            return parentMap.values();
+        }
+    }
+
+    @Override
+    public Set<Entry<String, String>> entrySet() {
+        synchronized (parentMap) {
+            return parentMap.entrySet();
+        }
+    }
+}
