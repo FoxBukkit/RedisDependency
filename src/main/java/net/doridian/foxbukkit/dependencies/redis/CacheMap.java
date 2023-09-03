1777437f9c472d55c487149259268ad4adf880f8
Doridian <git@doridian.net>
2023-09-02 23:29:46 -0700

    fix license

--- src/main/java/net/doridian/foxbukkit/dependencies/redis/CacheMap.java
+++ src/main/java/net/doridian/foxbukkit/dependencies/redis/CacheMap.java
@@ -1,20 +1,21 @@
-/**
- * This file is part of RedisDependency.
+/*
+ * redis-dependency - ${project.description}
+ * Copyright © ${year} Doridian (git@doridian.net)
  *
- * RedisDependency is free software: you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * RedisDependency is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with RedisDependency.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.foxelbox.dependencies.redis;
+package net.doridian.foxbukkit.dependencies.redis;
 
 import java.util.*;
 
